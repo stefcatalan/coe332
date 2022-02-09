92399387272c138a25abe6e16a3dd065172c1c7e
Stefanie Adriana Catalan <stefanie.catalan@utexas.edu>
2022-02-09 15:51:27 -0600

    outline of part 1

---
+++ homework02/generateCoordinates.py
@@ -0,0 +1,9 @@
+# randomly generate latitude, longitude, compositions for landing sites
+
+# generate 5 random pairs of latitudes in decimal notation
+
+# randomly choose a meteorite composition from list
+
+# assemble data into a dictionary
+
+# save data into a JSON file
