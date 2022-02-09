b4fd3c68330753b48c86c33eea09e6851719069b
stefcatalan <stefanie.catalan@utexas.edu>
2022-02-09 15:54:18 -0600

    outline of part 2

---
+++ homework02/calculateVisit.py
@@ -0,0 +1,15 @@
+# reads in the meteorite JSON file & calculates time required to visit sites
+
+# use json library to read data generated in part 1 & store as dictionary
+
+# assume robot starts at latitude/longitude
+
+# assume robot visits the sites
+
+# assume max robot speed is 10 km/h
+
+# assume mars is a sphere & use great-circle algorithm
+
+# robot stops to take sample
+
+# trip is over after sampling the meteorite
